6bafca4a3f8505fc6aac66d846fa17ab003a9299
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-29 23:55:36 +1100

    @test

--- src/test/java/com/_if/dateutils/MyDateTest.java
+++ src/test/java/com/_if/dateutils/MyDateTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MyDateTest {
+	
+	@Test
 	public void testToString(){
 		MyDate date = new MyDate(2000, 12, 31);
 		Assert.assertEquals("31 12 2000", date.toString());
