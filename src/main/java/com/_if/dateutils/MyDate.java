06f5aa10fa6518998a979d20e0a649532b9f761d
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-29 23:56:18 +1100

    MyDate toString initial impl.

--- src/main/java/com/_if/dateutils/MyDate.java
+++ src/main/java/com/_if/dateutils/MyDate.java
@@ -1,5 +1,17 @@
 package com._if.dateutils;
 
 public class MyDate {
-
+	private int yyyy;
+	private int mm;
+	private int dd;
+	
+	public MyDate(int yyyy, int mm, int dd){
+		this.yyyy = yyyy;
+		this.mm = mm;
+		this.dd = dd;
+	}
+	
+	public String toString(){
+		return String.format("%d %d %d", dd, mm, yyyy);
+	}
 }
