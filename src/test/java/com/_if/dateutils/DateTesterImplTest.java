3441228be420c90683de866e1d8251570377f939
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 11:58:57 +1100

    testing valid year range - between 1900 and 2010 inclusive

--- src/test/java/com/_if/dateutils/DateTesterImplTest.java
+++ src/test/java/com/_if/dateutils/DateTesterImplTest.java
@@ -22,4 +22,19 @@ public class DateTesterImplTest {
 	public void whenYearGreaterThan2010ReturnFalse(){
 		Assert.assertFalse(tester.test(2011, 1, 1));
 	}
+	
+	@Test
+	public void whenYearBetween1900And2010ReturnTrue(){
+		Assert.assertTrue(tester.test(1999, 1, 1));
+	}
+	
+	@Test
+	public void whenYear1900ReturnTrue(){
+		Assert.assertTrue(tester.test(1900, 1, 1));
+	}
+	
+	@Test
+	public void whenYear2010ReturnTrue(){
+		Assert.assertTrue(tester.test(2010, 1, 1));
+	}
 }
