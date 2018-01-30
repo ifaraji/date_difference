d4276a7943ca235dda16d8af1bef0c3d0e6c4718
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 11:59:37 +1100

    valid year range - between 1900 and 2010 inclusive

--- src/main/java/com/_if/dateutils/DateTesterImpl.java
+++ src/main/java/com/_if/dateutils/DateTesterImpl.java
@@ -3,8 +3,8 @@ package com._if.dateutils;
 public class DateTesterImpl implements DateTester{
 
 	public boolean test(int yyyy, int mm, int dd) {
-		// TODO Auto-generated method stub
-		return false;
+		if (yyyy < 1900 || yyyy > 2010) return false;
+		return true;
 	}
 
 }
