68e68c43070d0a17b467b767ba7ad3c58d2cf4ba
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 23:39:03 +1100

    more testing for DateDelta.diffDays

--- src/test/java/com/_if/dateutils/DateDeltaTest.java
+++ src/test/java/com/_if/dateutils/DateDeltaTest.java
@@ -19,4 +19,44 @@ public class DateDeltaTest {
 		int diff = delta.diffDays(date2, date1);
 		Assert.assertEquals(1, diff);
 	}
+	
+	@Test
+	public void testDiffDaysWith2leapYears(){
+		MyDate date1 = new MyDate(2000, 12, 31);
+		MyDate date2 = new MyDate(2004, 12, 31);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(1461, diff);
+	}
+	
+	@Test
+	public void testDiffDaysWithOnlyDifferentDays(){
+		MyDate date1 = new MyDate(2005, 2, 4);
+		MyDate date2 = new MyDate(2005, 2, 7);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(3, diff);
+	}
+	
+	@Test
+	public void testDiffDaysWith1DayDiffAcross2Months(){
+		MyDate date1 = new MyDate(2005, 2, 28);
+		MyDate date2 = new MyDate(2005, 3, 1);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(1, diff);
+	}
+	
+	@Test
+	public void testDiffDaysWithOneYearDifference(){
+		MyDate date1 = new MyDate(2005, 4, 1);
+		MyDate date2 = new MyDate(2006, 4, 1);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(365, diff);
+	}
+	
+	@Test
+	public void testDiffDaysWithOneYearAndOneDayDifference(){
+		MyDate date1 = new MyDate(2005, 4, 1);
+		MyDate date2 = new MyDate(2006, 4, 2);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(366, diff);
+	}
 }
