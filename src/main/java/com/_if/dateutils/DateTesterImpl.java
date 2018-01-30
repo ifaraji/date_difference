6b95b3bfa0f1cc94a0651f68ea8b3fee5c911a98
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 13:18:31 +1100

    valid day - greater than 1

--- src/main/java/com/_if/dateutils/DateTesterImpl.java
+++ src/main/java/com/_if/dateutils/DateTesterImpl.java
@@ -5,6 +5,7 @@ public class DateTesterImpl implements DateTester{
 	public boolean test(int yyyy, int mm, int dd) {
 		if (yyyy < 1900 || yyyy > 2010) return false;
 		if (mm < 1 || mm > 12) return false;
+		if (dd < 1) return false;
 		return true;
 	}
 
