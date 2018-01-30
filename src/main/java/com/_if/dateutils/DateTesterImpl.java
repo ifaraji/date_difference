78d5c94379dbae12f93e9a5125311d5e06a07b11
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 12:02:36 +1100

    valid month range - between 1 and 12

--- src/main/java/com/_if/dateutils/DateTesterImpl.java
+++ src/main/java/com/_if/dateutils/DateTesterImpl.java
@@ -4,6 +4,7 @@ public class DateTesterImpl implements DateTester{
 
 	public boolean test(int yyyy, int mm, int dd) {
 		if (yyyy < 1900 || yyyy > 2010) return false;
+		if (mm < 1 || mm > 12) return false;
 		return true;
 	}
 
