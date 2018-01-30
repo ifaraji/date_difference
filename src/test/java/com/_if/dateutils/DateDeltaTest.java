d066dcc9dd766b098982490049f83ef48eaafd80
Ideen Faraji <ideen.faraji@servian.com.au>
2018-01-30 16:22:47 +1100

    testing DateDelta.diffDays

--- src/test/java/com/_if/dateutils/DateDeltaTest.java
+++ src/test/java/com/_if/dateutils/DateDeltaTest.java
@@ -1,5 +1,22 @@
 package com._if.dateutils;
 
-public class DateDeltaTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+public class DateDeltaTest {
+	private DateDelta delta;
+	
+	@Before
+	public void init(){
+		delta = new DateDelta();
+	}
+	
+	@Test
+	public void testDiffDays(){
+		MyDate date1 = new MyDate(2000, 12, 31);
+		MyDate date2 = new MyDate(2001, 01, 01);
+		int diff = delta.diffDays(date2, date1);
+		Assert.assertEquals(1, diff);
+	}
 }
